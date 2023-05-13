37e60ef3744cb823f5d8be9eb6b36da2ca39859b
Minase11 <minase.m.mamo@gmail.com>
2023-05-13 19:57:36 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+import Stripe from "stripe";
+
+const stripe = new Stripe(
+  "sk_test_51JcXQfB6n4gamyDe7lOogbmENv96LWIbYox0BGSYguaTdxeCgE8egVpQRSlLlov6f7aKV1RwWT5H80tYso7l5VU300WW4losXj",
+  {
+    apiVersion: "2022-11-15",
+  }
+);
+
+// App Config
+const app = express();
+const port = process.env.PORT || 8001;
+
+// Middlewares
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+// API Endpoints
+app.get("/", (req, res) => res.status(200).send("Hello World!"));
+
+app.post("/payments/create", async (req, res) => {
+  const total = req.query.total;
+  console.log(`Payment Received BOOM!!!! For this amount >>> ${total}`);
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total,
+    currency: "usd",
+  });
+  res.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+// Listeners
+app.listen(port, () => console.log(`Listening on port ${port}`));
